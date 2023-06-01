44b89b12dc3286649e1349cb996acdcc27cf1973
Annette29 <annette.irungu@jkuat.ac.ke>
2023-06-01 11:42:11 +0900

    Code before adding signup page

--- idea_four/process_quiz.php
+++ idea_four/process_quiz.php
@@ -1,84 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Connect to the MySQL database
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "second_quiz";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Prepare the statement for inserting quiz responses
-    $stmt = $conn->prepare("INSERT INTO quiz_questions (question, option1, option2, option3, option4) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $question, $option1, $option2, $option3, $option4);
-
-    // Process the quiz answers
-    $answers = [
-        $_POST['q1'],
-        $_POST['q2'],
-        $_POST['q3'],
-        $_POST['q4'],
-        $_POST['q5'],
-        $_POST['q6'],
-        $_POST['q7'],
-    ];
-
-    $question = "What is your favorite ice cream flavor?";
-    $option1 = "Chocolate";
-    $option2 = "Vanilla";
-    $option3 = "Strawberry";
-    $option4 = "Mint Chocolate Chip";
-    $stmt->execute();
-
-    $question = "What is your favorite movie genre?";
-    $option1 = "Action";
-    $option2 = "Comedy";
-    $option3 = "Drama";
-    $option4 = "Horror";
-    $stmt->execute();
-
-    $question = "What is your favorite TV show?";
-    $option1 = "Friends";
-    $option2 = "The Office";
-    $option3 = "Game of Thrones";
-    $option4 = "Stranger Things";
-    $stmt->execute();
-
-    $question = "What is your favorite book genre?";
-    $option1 = "Romance";
-    $option2 = "Mystery";
-    $option3 = "Science Fiction";
-    $option4 = "Fantasy";
-    $stmt->execute();
-
-    $question = "What is your favorite hobby?";
-    $option1 = "Reading";
-    $option2 = "Watching Movies";
-    $option3 = "Playing Video Games";
-    $option4 = "Traveling";
-    $stmt->execute();
-
-    $question = "What is your favorite animal?";
-    $option1 = "Dog";
-    $option2 = "Cat";
-    $option3 = "Bird";
-    $option4 = "Fish";
-    $stmt->execute();
-
-    $question = "What is your favorite season?";
-    $option1 = "Spring";
-    $option2 = "Summer";
-    $option3 = "Fall";
-    $option4 = "Winter";
-    $stmt->execute();
-
-    $stmt->close();
-    $conn->close();
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -86,23 +5,111 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="pimg1">
-    <div class="ptext1">
+    <div class="pimg1">
+        <div class="ptext">
+            <span class="boder">
+                Here are your answers!
+            </span>
+        </div>
+    </div>
+    <section class="section section-dark">
+        <div class="ptext1">
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                // Connect to the MySQL database
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "second_quiz";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                // Prepare the statement for inserting quiz responses
+                $stmt = $conn->prepare("INSERT INTO quiz_questions (question, option1, option2, option3, option4) VALUES (?, ?, ?, ?, ?)");
+                $stmt->bind_param("sssss", $question, $option1, $option2, $option3, $option4);
+
+                // Process the quiz answers
+                $answers = [
+                    $_POST['q1'],
+                    $_POST['q2'],
+                    $_POST['q3'],
+                    $_POST['q4'],
+                    $_POST['q5'],
+                    $_POST['q6'],
+                    $_POST['q7'],
+                ];
+
+                $questions = [
+                    "What is your favorite ice cream flavor?",
+                    "What is your favorite movie genre?",
+                    "What is your favorite TV show?",
+                    "What is your favorite book genre?",
+                    "What is your favorite hobby?",
+                    "What is your favorite animal?",
+                    "What is your favorite season?"
+                ];
+
+                $options = [
+                    ["Chocolate", "Vanilla", "Strawberry", "Mint Chocolate Chip"],
+                    ["Action", "Comedy", "Drama", "Horror"],
+                    ["Friends", "The Office", "Game of Thrones", "Stranger Things"],
+                    ["Romance", "Mystery", "Science Fiction", "Fantasy"],
+                    ["Reading", "Watching Movies", "Playing Video Games", "Traveling"],
+                    ["Dog", "Cat", "Bird", "Fish"],
+                    ["Spring", "Summer", "Fall", "Winter"]
+                ];
+
+                for ($i = 0; $i < count($questions); $i++) {
+                    $question = $questions[$i];
+                    $answerIndex = intval($answers[$i]);
+
+                    // Check if the answer index is within the valid range
+                    if ($answerIndex >= 0 && $answerIndex < count($options[$i])) {
+                        $questionOptions = $options[$i];
+                        $option1 = $questionOptions[$answerIndex];
+                    } else {
+                        $option1 = "No answer provided";
+                    }
+
+                    $option2 = $options[$i][0];
+                    $option3 = $options[$i][1];
+                    $option4 = $options[$i][2];
+
+                    $stmt->execute();
+
+                    echo "<h2>Question:</h2>";
+                    echo "<p>" . $question . "</p>";
+                    echo "<h2>Answer:</h2>";
+                    echo "<p>" . $option1 . "</p>";
+                    echo "<br>";
+                }
+
+                $stmt->close();
+                $conn->close();
+            } else {
+                echo "No quiz answers submitted.";
+            }
+            ?>
+        </div>
+    </section>
+    <div class="pimg7">
+        <div class="ptext">
+            <span class="boder trans">
+                Thank you for playing!
+            </span>
+        </div>
+    </div>
+    <form action="open_quiz.html" method="POST">
+    <div class="pimg1">
+      <div class="ptext">
         <span class="boder">
-          Here are your answers!
+          <input type="submit" value="Want to give it another shot?" class="myButton">
         </span>
+      </div>
     </div>
-    <div class=".ptext2">
-      <?php
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-          foreach ($answers as $answer) {
-            echo $answer . '<br>';
-          }
-        } else {
-          echo "No quiz answers submitted.";
-        }
-      ?>
-    </div>
-  </div>
+  </form>
 </body>
 </html>
